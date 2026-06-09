c47aeb8b6721b24ceb7bfb21504b126d7e60d2af
Elias Guardado <eliasjguardado2001@gmail.com>
2026-06-09 15:20:33 -0600

    add: state in reactive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
+import {ref, reactive} from 'vue'
 import Header from "./components/Header.vue"
+
+const cantidad = ref(0);
+
+
+
+function handleChange(e) {
+  cantidad.value = Number(e.target.value);
+}
+
 
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div>
+      <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        @input="handleChange"
+      />
+
+      {{cantidad}}
+    </div>
   </div>
 </template>
