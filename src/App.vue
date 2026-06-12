66290fdc81b4feeab232b9320e7477736d413075
Elias Guardado <eliasjguardado2001@gmail.com>
2026-06-12 16:12:39 -0600

    Add: creating a button component, passing props, adding a dropdown menu, and creating a payment summary section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import {ref, computed} from 'vue'
 import Header from "./components/Header.vue"
+import Button from './components/Button.vue';
 
 const cantidad = ref(10000);
-
+const meses = ref(6)
 const MIN = 0;
 const MAX = 20000;
 const STEP = 100; 
@@ -46,19 +47,15 @@ const handleChangeIncremento = () => {
     <Header />
 
     <div class="flex justify-between mt-10">
-      <button
-      class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500
-      rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 
-      hover:ring-lime-500 text-white text-2xl"
-      @click="handleChangeDecremento"
-      >-</button>
+     <Button
+      :operador="'-'"
+      :fn="handleChangeDecremento"
+     />
+     <Button
+      :operador="'+'"
+      :fn="handleChangeIncremento"
+     />
 
-      <button
-      class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500
-      rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 
-      hover:ring-lime-500 text-white text-2xl"
-      @click="handleChangeIncremento"
-      >+</button>
     </div>
 
     <div>
@@ -72,6 +69,26 @@ const handleChangeIncremento = () => {
       />
 
       <p class="text-center my-10 text-5xl font-extrabold text-indigo-600"> {{formatearDinero}}</p>
+      <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span class="text-indigo-600">Plazo </span> a pagar.
+      </h2>
+      <select class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold
+      text-gray-500 mt-5"
+      :value="meses"
+      v-model.number="meses"
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option>
+      </select>
+    </div>
+    <div class="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span class="text-indigo-600">de pagos</span>
+        </h2>
+        <p class="text-xl text-gray-500 text-center font-bold">{{meses}} Meses</p>
+        <p class="text-xl text-gray-500 text-center font-bold">Total a pagar: </p>
+        <p class="text-xl text-gray-500 text-center font-bold">Mensuales </p>
     </div>
   </div>
 </template>
